a1265cc9105ea419e553dc2a5fa73f10b6121a5e
Scott Griffy <scottgriffy@gmail.com>
2014-01-25 06:00:17 -0800

    textures and shizz

---
+++ Assets/Scripts/Telepuerto.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Telepuerto : MonoBehaviour {
+	public string scene;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (scene != null && col.collider.CompareTag("guy"))
+			Application.LoadLevel(scene);
+	}
+}
