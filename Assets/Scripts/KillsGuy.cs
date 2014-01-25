8b271d342d4fd0604aac39e6e86972351948f2d1
Scott Griffy <scottgriffy@gmail.com>
2014-01-25 14:37:09 -0800

    more tons of shit

---
+++ Assets/Scripts/KillsGuy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillsGuy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.collider.CompareTag("guy") || col.collider.CompareTag("animal"))
+		{
+			if (transform.position.y > col.gameObject.transform.position.y+2)
+				Destroy(col.gameObject);
+		}
+	}
+}
