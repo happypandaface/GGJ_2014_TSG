62dc35e9144a7a585a6e7971c67b06d2cca351cf
Scott Griffy <scottgriffy@gmail.com>
2014-01-25 20:47:04 -0800

    fade

---
+++ Assets/Scripts/fade.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fade : MonoBehaviour {
+	public float start;
+	public float end;
+	public string nextLevel;
+	private float alpha = 0;
+	// Use this for initialization
+	void Start () {
+		alpha = start;
+		renderer.material.color = new Color(1, 1, 1, alpha);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(0, 0, -8);
+		if (nextLevel != null && !nextLevel.Equals("") && Mathf.Abs (end-renderer.material.color.a) < Time.deltaTime)
+			Application.LoadLevel(nextLevel);
+		if (end < renderer.material.color.a)
+			alpha -= Time.deltaTime;
+		else
+			alpha += Time.deltaTime;
+		renderer.sharedMaterial.color = new Color(1, 1, 1, alpha);
+	}
+}
