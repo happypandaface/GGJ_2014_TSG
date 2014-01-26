a0077b74827153c2e424d82b8d9e05b42d3e7f67
Scott Griffy <scottgriffy@gmail.com>
2014-01-25 16:01:29 -0800

    saving before the apocalypse

---
+++ Assets/Scripts/Dies.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dies : MonoBehaviour {
+	public Transform spatterTrans;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Die()
+	{
+		for (int i = 0; i < 10; ++i)
+		{
+			Transform spatter = (Transform)Instantiate(spatterTrans, transform.position, Quaternion.Euler(0, 0, Random.value*Mathf.PI*2));
+			spatter.rigidbody2D.velocity = Random.insideUnitCircle*5;
+			spatter.rigidbody2D.angularVelocity = Random.value;
+		}
+	}
+}
---
+++ Assets/Scripts/Kills.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kills : MonoBehaviour {
+	public AudioClip deathSound;
+	public AudioSource deathSource;
+	// Use this for initialization
+	void Start ()
+	{
+		deathSource = gameObject.AddComponent<AudioSource>();
+		deathSource.clip = deathSound;
+		deathSource.loop = false;
+		deathSource.playOnAwake = false;
+		deathSource.volume = 1f;
+		deathSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	public void Die()
+	{
+		print ("death sound");
+		deathSource.Play();
+	}
+}
